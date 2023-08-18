4b085f4b4e51310c0c23ad65929f54466d2bb515
justnaf <naufalaf86@gmail.com>
2023-08-18 23:25:05 +0700

    Edit Navigation

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -8,15 +8,22 @@
         <div class="my-2 bg-gray-600 h-[1px]"></div>
     </div>
     <a href="{{ route('dashboard') }}">
-        <div
-            class="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white">
-            <i class="bi bi-house-door-fill"></i>
-            <span class="text-[15px] ml-4 text-gray-200 font-bold">Beranda</span>
-        </div>
+        @if (request()->routeIs('dashboard'))
+            <div class="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer bg-blue-600 text-white">
+                <i class="bi bi-house-door-fill"></i>
+                <span class="text-[15px] ml-4 text-gray-200 font-bold">Beranda</span>
+            </div>
+        @else
+            <div
+                class="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white">
+                <i class="bi bi-house-door-fill"></i>
+                <span class="text-[15px] ml-4 text-gray-200 font-bold">Beranda</span>
+            </div>
+        @endif
     </a>
     <div class="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white">
         <i class="bi bi-bookmark-fill"></i>
-        <span class="text-[15px] ml-4 text-gray-200 font-bold">Informasi</span>
+        <span class="text-[15px] ml-4 text-gray-200 font-bold">Presensi</span>
     </div>
     <div class="my-4 bg-gray-600 h-[1px]"></div>
     <div class="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white"
@@ -40,30 +47,58 @@
             Pengumpulan
         </h1>
     </div>
-    <div class="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white"
-        onclick="dropdown2()">
-        <i class="bi bi-person-circle"></i>
-        <div class="flex justify-between w-full items-center">
-            <span class="text-[15px] ml-4 text-gray-200 font-bold">{{ Auth::user()->name }}</span>
-            <span class="text-sm rotate-180" id="arrow2">
-                <i class="bi bi-chevron-down"></i>
-            </span>
+    @if (request()->routeIs('profile.*'))
+        <div class="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer bg-blue-600 text-white"
+            onclick="dropdown2()">
+            <i class="bi bi-person-circle"></i>
+            <div class="flex justify-between w-full items-center">
+                <span class="text-[15px] ml-4 text-gray-200 font-bold">{{ Auth::user()->name }}</span>
+                <span class="text-sm rotate-180" id="arrow2">
+                    <i class="bi bi-chevron-down"></i>
+                </span>
+            </div>
         </div>
-    </div>
-    <div class="text-left text-sm mt-2 w-4/5 mx-auto text-gray-200 font-bold" id="submenu2">
-        <a href="{{ route('profile.edit') }}">
-            <h1 class="cursor-pointer p-2 hover:bg-blue-600 rounded-md mt-1">
+        <div class="text-left text-sm mt-2 w-4/5 mx-auto text-gray-200 font-bold" id="submenu2">
+            <a href="{{ route('profile.edit') }}">
+                <h1 class="cursor-pointer p-2 hover:bg-blue-600 rounded-md mt-1">
 
-                Profile Edit
-            </h1>
-        </a>
-        <form action="{{ route('logout') }}" method="POST">
-            @csrf
-            <button type="submit" class="w-full items-start cursor-pointer p-2 hover:bg-blue-600 rounded-md mt-1">
-                <i class="bi bi-box-arrow-in-right"></i>
-                Logout
-            </button>
-        </form>
-    </div>
+                    Profile Edit
+                </h1>
+            </a>
+            <form action="{{ route('logout') }}" method="POST">
+                @csrf
+                <button type="submit" class="w-full items-start cursor-pointer p-2 hover:bg-blue-600 rounded-md mt-1">
+                    <i class="bi bi-box-arrow-in-right"></i>
+                    Logout
+                </button>
+            </form>
+        </div>
+    @else
+        <div class="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white"
+            onclick="dropdown2()">
+            <i class="bi bi-person-circle"></i>
+            <div class="flex justify-between w-full items-center">
+                <span class="text-[15px] ml-4 text-gray-200 font-bold">{{ Auth::user()->name }}</span>
+                <span class="text-sm rotate-180" id="arrow2">
+                    <i class="bi bi-chevron-down"></i>
+                </span>
+            </div>
+        </div>
+        <div class="text-left text-sm mt-2 w-4/5 mx-auto text-gray-200 font-bold" id="submenu2">
+            <a href="{{ route('profile.edit') }}">
+                <h1 class="cursor-pointer p-2 hover:bg-blue-600 rounded-md mt-1">
+
+                    Profile Edit
+                </h1>
+            </a>
+            <form action="{{ route('logout') }}" method="POST">
+                @csrf
+                <button type="submit" class="w-full items-start cursor-pointer p-2 hover:bg-blue-600 rounded-md mt-1">
+                    <i class="bi bi-box-arrow-in-right"></i>
+                    Logout
+                </button>
+            </form>
+        </div>
+    @endif
 
 </div>
