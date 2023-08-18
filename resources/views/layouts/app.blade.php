2a9ea4cc3364fd0238d1eb6cf1cf9ba5512e83d7
justnaf <naufalaf86@gmail.com>
2023-08-18 22:43:01 +0700

    Pembaharuan Tampilan Sidebar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,62 +1,67 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-    {{-- <script src="https://cdn.tailwindcss.com"></script> --}}
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.0/font/bootstrap-icons.css" />
-    <!-- Scripts -->
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-</head>
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.0/font/bootstrap-icons.css" />
+        <!-- Scripts -->
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    </head>
 
-<body class="font-sans antialiased">
-    <div class="flex w-screen h-screen bg-gray-100 dark:bg-gray-900">
-        @include('layouts.navigation')
+    <body class="font-sans antialiased">
+        <div class="flex w-screen h-screen bg-gray-100 dark:bg-gray-900">
+            @include('layouts.navigation')
 
-        <div class="grow text-black" id="content">
-            <div class="flex flex-col mt-3 px-3">
-                <!-- Page Heading -->
-                @if (isset($header))
-                    <header class="flex items-center bg-white dark:bg-gray-800 shadow">
-                        <ion-icon id="menuToggleBtn" name="menu" class="text-3xl cursor-pointer ml-5">
-                        </ion-icon>
-                        <div class="max-w-7xl py-6 px-4 sm:px-6 lg:px-8">
-                            {{ $header }}
-                        </div>
-                    </header>
-                @endif
+            <div class="grow text-black" id="content">
+                <div class="flex flex-col mt-3 px-3">
+                    <!-- Page Heading -->
+                    @if (isset($header))
+                        <header class="flex items-center bg-white dark:bg-gray-800 shadow">
+                            <ion-icon id="menuToggleBtn" name="menu" class="text-3xl cursor-pointer ml-5">
+                            </ion-icon>
+                            <div class="max-w-7xl py-6 px-4 sm:px-6 lg:px-8">
+                                {{ $header }}
+                            </div>
+                        </header>
+                    @endif
 
-                <!-- Page Content -->
-                <main>
-                    {{ $slot }}
-                </main>
+                    <!-- Page Content -->
+                    <main>
+                        {{ $slot }}
+                    </main>
+                </div>
             </div>
         </div>
-    </div>
-</body>
+    </body>
 
-<script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
-<script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
-<script>
-    let toggleButton = document.querySelector("#menuToggleBtn").addEventListener("click", () => {
-        let sidebar = document.querySelector("#sidebar");
-        sidebar.classList.toggle("translate-x-[-100%]");
-        sidebar.classList.toggle("sm:w-0");
-    });
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+    <script>
+        let toggleButton = document.querySelector("#menuToggleBtn").addEventListener("click", () => {
+            let sidebar = document.querySelector("#sidebar");
+            sidebar.classList.toggle("translate-x-[-100%]");
+            sidebar.classList.toggle("sm:w-0");
+        });
 
-    function dropdown() {
-        document.querySelector("#submenu").classList.toggle("hidden");
-        document.querySelector("#arrow").classList.toggle("rotate-0");
-    }
-    dropdown();
-</script>
+        function dropdown1() {
+            document.querySelector("#submenu1").classList.toggle("hidden");
+            document.querySelector("#arrow1").classList.toggle("rotate-0");
+        }
+        dropdown1();
+
+        function dropdown2() {
+            document.querySelector("#submenu2").classList.toggle("hidden");
+            document.querySelector("#arrow2").classList.toggle("rotate-0");
+        }
+        dropdown2();
+    </script>
 
 </html>
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,4 +1,4 @@
-<div class="w-[250px] top-0 bottom-0 lg:left-0 p-2 duration-300 overflow-hidden bg-gradient-to-t from-gray-600 to-gray-900"
+<div class="w-[250px] top-0 bottom-0 lg:left-0 p-2 duration-300 overflow-hidden bg-gradient-to-t from-gray-600 to-gray-900 h-full"
     id="sidebar">
     <div class="text-gray-100 text-xl mt-6">
         <div class="p-2.5 flex items-center">
@@ -7,26 +7,29 @@
         </div>
         <div class="my-2 bg-gray-600 h-[1px]"></div>
     </div>
-    <div class="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white">
-        <i class="bi bi-house-door-fill"></i>
-        <span class="text-[15px] ml-4 text-gray-200 font-bold">Beranda</span>
-    </div>
+    <a href="{{ route('dashboard') }}">
+        <div
+            class="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white">
+            <i class="bi bi-house-door-fill"></i>
+            <span class="text-[15px] ml-4 text-gray-200 font-bold">Beranda</span>
+        </div>
+    </a>
     <div class="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white">
         <i class="bi bi-bookmark-fill"></i>
         <span class="text-[15px] ml-4 text-gray-200 font-bold">Informasi</span>
     </div>
     <div class="my-4 bg-gray-600 h-[1px]"></div>
     <div class="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white"
-        onclick="dropdown()">
+        onclick="dropdown1()">
         <i class="bi bi-chat-left-text-fill"></i>
         <div class="flex justify-between w-full items-center">
-            <span class="text-[15px] ml-4 text-gray-200 font-bold">Tugas</span>
-            <span class="text-sm rotate-180" id="arrow">
+            <span class="text-[15px] ml-4 text-gray-200 font-bold">Underconstruction</span>
+            <span class="text-sm rotate-180" id="arrow1">
                 <i class="bi bi-chevron-down"></i>
             </span>
         </div>
     </div>
-    <div class="text-left text-sm mt-2 w-4/5 mx-auto text-gray-200 font-bold" id="submenu">
+    <div class="text-left text-sm mt-2 w-4/5 mx-auto text-gray-200 font-bold" id="submenu1">
         <h1 class="cursor-pointer p-2 hover:bg-blue-600 rounded-md mt-1">
             Absensi
         </h1>
@@ -37,8 +40,30 @@
             Pengumpulan
         </h1>
     </div>
-    <div class="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white">
-        <i class="bi bi-box-arrow-in-right"></i>
-        <span class="text-[15px] ml-4 text-gray-200 font-bold">Logout</span>
+    <div class="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white"
+        onclick="dropdown2()">
+        <i class="bi bi-person-circle"></i>
+        <div class="flex justify-between w-full items-center">
+            <span class="text-[15px] ml-4 text-gray-200 font-bold">{{ Auth::user()->name }}</span>
+            <span class="text-sm rotate-180" id="arrow2">
+                <i class="bi bi-chevron-down"></i>
+            </span>
+        </div>
     </div>
+    <div class="text-left text-sm mt-2 w-4/5 mx-auto text-gray-200 font-bold" id="submenu2">
+        <a href="{{ route('profile.edit') }}">
+            <h1 class="cursor-pointer p-2 hover:bg-blue-600 rounded-md mt-1">
+
+                Profile Edit
+            </h1>
+        </a>
+        <form action="{{ route('logout') }}" method="POST">
+            @csrf
+            <button type="submit" class="w-full items-start cursor-pointer p-2 hover:bg-blue-600 rounded-md mt-1">
+                <i class="bi bi-box-arrow-in-right"></i>
+                Logout
+            </button>
+        </form>
+    </div>
+
 </div>
